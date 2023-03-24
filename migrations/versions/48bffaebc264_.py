23bea6761d5033c3d4f03aa4a2066cee20c539d4
Simencassiman <simen.cassiman@student.kuleuven.be>
2023-03-24 19:42:28 +0100

    mend

---
+++ migrations/versions/48bffaebc264_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 48bffaebc264
+Revises: 148f021faa58
+Create Date: 2023-03-21 19:47:29.598219
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = "48bffaebc264"
+down_revision = "148f021faa58"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
